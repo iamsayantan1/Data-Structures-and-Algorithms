8e4ce21f5db3bb0907b2521a6677313e26d0df84
Sayantan Das <sdas45758@gmail.com>
2023-08-17 01:19:31 +0530

    Commiting Tower_Of_Hanoi.c

---
+++ C/Tower_Of_Hanoi.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+
+void Tower_Of_Hanoi(int no_of_Plates, char source, char destination, char aux)    {
+    if(no_of_Plates < 1)   {
+        printf("Illogical statement...");
+        return;
+    }
+
+    if (no_of_Plates == 1)
+    {
+        printf("%c -> %c\n",source, destination);
+        return;
+    }
+
+    Tower_Of_Hanoi(no_of_Plates-1, source, aux, destination);
+    printf("%c -> %c\n",source, destination);
+    Tower_Of_Hanoi(no_of_Plates-1, aux, destination, source);
+}
+
+int main()  {
+    printf("Enter no of plates for tower of hanoi problem: ");
+    int no_of_Plates;
+    scanf("%d",&no_of_Plates);
+    printf("Transitions... ");
+    Tower_Of_Hanoi(no_of_Plates, 'A', 'C', 'B');
+    return 0;
+}
