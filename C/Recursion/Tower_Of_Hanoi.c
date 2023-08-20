1c76052da09e474c5318881052cc909bb1d3c146
Sayantan Das <sdas45758@gmail.com>
2023-08-20 20:15:17 +0530

    Towe_Of_Hanoi.c is moved to Recursion Subfolder

--- C/Recursion/Tower_Of_Hanoi.c
+++ C/Recursion/Tower_Of_Hanoi.c
@@ -1,11 +1,6 @@
 #include<stdio.h>
 
 void Tower_Of_Hanoi(int no_of_Plates, char source, char destination, char aux)    {
-    if(no_of_Plates < 1)   {
-        printf("Illogical statement...");
-        return;
-    }
-
     if (no_of_Plates == 1)
     {
         printf("%c -> %c\n",source, destination);
@@ -21,7 +16,15 @@ int main()  {
     printf("Enter no of plates for tower of hanoi problem: ");
     int no_of_Plates;
     scanf("%d",&no_of_Plates);
+    if(no_of_Plates < 0)   {
+        printf("Invalid input...");
+        return 0;
+    }
     printf("Transitions... ");
+    if(no_of_Plates == 0)   {
+        printf("Invalid input...");
+        return;
+    }
     Tower_Of_Hanoi(no_of_Plates, 'A', 'C', 'B');
     return 0;
 }
